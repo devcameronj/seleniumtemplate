80f25a34aefe0256adcada7a19193ca19359e639
Jyothi Cameron <devjyothicameron@gmail.com>
2023-06-06 04:53:16 -0400

    upded notes

--- seleniumtest.py
+++ seleniumtest.py
@@ -1,12 +1,11 @@
 # import time
-
+# import getpass
+# import sys
+# import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
-# element.send_keys(Keys.ENTER)  # Simulate pressing the Enter key
-# element.send_keys(Keys.TAB)  # Simulate pressing the Tab key
-# element.send_keys(Keys.CONTROL + 'a')  # Simulate pressing Ctrl+A
-# element.send_keys(Keys.CONTROL + 'c')  # Simulate pressing Ctrl+C
+
 
 # Specify the path to the ChromeDriver executable
 driver_path = 'Lib\site-packages\chromedriver'
@@ -17,12 +16,19 @@ service = Service(driver_path)
 # Create a WebDriver instance and pass the Service object
 driver = webdriver.Chrome(service=service)
 
-
+# Request the page to automate
 driver.get('https://www.jyothicameron.dev')
-# element = driver.find_element_by_id('input_id')
-# element.send_keys('username')
-# element.send_keys('password')
 
+# # Grab the element you want to interact with 
+# element = driver.find_element_by_id('input_id')
+
+# # Send data like the following: 
+# element.send_keys('username') # sends txt
+# element.send_keys('password') # sens text
+# element.send_keys(Keys.ENTER)  # Simulate pressing the Enter key
+# element.send_keys(Keys.TAB)  # Simulate pressing the Tab key
+# element.send_keys(Keys.CONTROL + 'a')  # Simulate pressing Ctrl+A
+# element.send_keys(Keys.CONTROL + 'c')  # Simulate pressing Ctrl+C
 
 # time.sleep(10)
 
